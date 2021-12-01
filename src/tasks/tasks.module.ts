987a72eb09819b5ec4d622ce544da0421a964822
ejaztoru999 <ejaztoru999@gmail.com>
2021-12-01 15:33:45 +0500

    auth module updated

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,11 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from "./task.repository";
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-
+import {AuthModule} from '../auth/auth.module'
 @Global()
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TaskRepository])
+    TypeOrmModule.forFeature([TaskRepository]),
+    AuthModule
   ] ,
   controllers: [TasksController],
   providers: [TasksService],
